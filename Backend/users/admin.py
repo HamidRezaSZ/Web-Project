46d6f893687c1e50ebcf45a95145fc8e803d445f
HamidReza Sadeghzadeh <hr.m.szh@gmail.com>
2024-02-05 17:14:50 +0330

    Update admin.py

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
+from .models import User
 
+
+@admin.register(User)
 class UserAdmin(DefaultUserAdmin):
     list_display = ('username', 'first_name', 'last_name',
                     'is_staff', 'is_superuser', 'is_active')
