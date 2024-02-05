a1e803620a954f936593b33b115c620e7f1d092c
HamidReza Sadeghzadeh <hr.m.szh@gmail.com>
2024-02-05 18:55:45 +0330

    Update settings.py

--- Backend/snapfood/settings.py
+++ Backend/snapfood/settings.py
@@ -140,6 +140,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+]
+
 CSRF_TRUSTED_ORIGINS = []
 
 CORS_ALLOW_HEADERS = (
